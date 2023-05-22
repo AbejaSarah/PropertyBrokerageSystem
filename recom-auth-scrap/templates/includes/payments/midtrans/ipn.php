b70343948ee84b766b9e7ead5e42774def74f1c8
Sarah <abejasara@gmail.com>
2023-05-22 14:49:56 +0100

    updated with basic popularity recommendation functionalities

--- recom-auth-scrap/templates/includes/payments/midtrans/ipn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$access_token=$_GET['access_token'];
-//print_r($_POST);
-if(isset($access_token)){
-    //payment_success_save_detail($access_token);
-}
-exit();
-?>
--- recom-auth-scrap/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# utils/__init__.py
-
-from .auth_utils import *
-from .config import *
-from .db import *
-from .scraper_utils import *
